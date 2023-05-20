27bd6b9b652330e0680ab4d18d8501cd428b346f
arnold-simanjuntak <arnolddominggo19@gmail.com>
2023-05-20 13:06:23 +0700

    refactor : rename helpers/readme.ts to helpers/repository.ts

--- apps/nextjs/src/helpers/__test__/readme.spec.ts
+++ apps/nextjs/src/helpers/__test__/readme.spec.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from "vitest";
 
-import { getPackageGithubUrl, getRawReadmeUrl } from "../readme";
+import { getPackageGithubUrl, getRawReadmeUrl } from "../repository";
 
 describe("getPackageGithubUrl", () => {
   test("should return the correct GitHub URL for a valid package", async () => {
--- apps/nextjs/src/helpers/index.ts
+++ apps/nextjs/src/helpers/index.ts
@@ -1,3 +1,3 @@
-export * from "./readme";
+export * from "./repository";
 export * from "./packages";
 export * from "./array";
