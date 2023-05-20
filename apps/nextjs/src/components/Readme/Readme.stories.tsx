52d054ae2bb06fd7aa8423a81e4dcafd491ec162
arnold-simanjuntak <arnolddominggo19@gmail.com>
2023-05-20 13:35:22 +0700

    fix : Readme stories

--- apps/nextjs/src/components/Readme/Readme.stories.tsx
+++ apps/nextjs/src/components/Readme/Readme.stories.tsx
@@ -5,7 +5,12 @@ import Readme from "./Readme";
 export const Default = () => {
   return (
     <div className="grid grid-cols-2 items-start gap-x-3">
-      <Readme>{"# Hello, World!\n"}</Readme>
+      <Readme
+        repoUrl="https://github.com/dominggo1999/package-info"
+        defaultBranch="main"
+      >
+        {"# Hello, World!\n"}
+      </Readme>
     </div>
   );
 };
