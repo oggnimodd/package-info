130a88d0cf3a66d69d106ae3cbd40117bdd1043b
oggnimodd <arnolddominggo19@gmail.com>
2023-10-11 23:06:03 +0700

    upgrade dependencies and fix React Markdown

--- apps/nextjs/src/components/Readme/Readme.tsx
+++ apps/nextjs/src/components/Readme/Readme.tsx
@@ -61,12 +61,16 @@ const Readme: React.FC<ReadmeProps> = ({
       remarkPlugins={[[remarkGfm]]}
       rehypePlugins={[rehypeRaw]}
       components={{
-        code({ node, inline, className, children, ...props }) {
+        code({ node, className, children, ref: _ref, ...props }) {
           const match = /language-(\w+)/.exec(className || "");
 
-          return !inline ? (
+          // TODO: this is a hack to silent type error
+          const ref = _ref as any;
+
+          return match ? (
             <SyntaxHighlighter
               {...props}
+              ref={ref}
               style={oneDark}
               language={match ? match[1] : "bash"}
               PreTag="div"
@@ -79,11 +83,19 @@ const Readme: React.FC<ReadmeProps> = ({
             </code>
           );
         },
-        li({ node, checked, children: liChildren, className, ...props }) {
-          if (checked !== null && checked !== undefined) {
+        li({ node, children: liChildren, className, ...props }) {
+          // li is a checkbox if the first element is and input with type checkbox
+          const isCheckbox =
+            node?.children[0]?.type === "element" &&
+            node?.children[0]?.properties.type === "checkbox";
+
+          if (isCheckbox) {
+            const checked =
+              node?.children[0]?.type === "element" &&
+              node?.children[0]?.properties.checked;
             return (
               <li {...props} className={clsx(className, "flex gap-x-3")}>
-                <Checkbox isSelected={checked}>{liChildren.slice(1)}</Checkbox>
+                <Checkbox isSelected={Boolean(checked)}>{liChildren}</Checkbox>
               </li>
             );
           }
